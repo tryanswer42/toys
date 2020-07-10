11c1ba5e8b05036232b4a541140c1eed631a5503
Cédric M <ced@test>
2020-07-10 08:05:15 +0200

    -Done :Basic config and import done        Defining domains  & relationships done        Welkome page view in progresse(add checkboxes)        List and update checkbox done        Services done        Working project (answer to questions) done        EntityGraph Added -Busy making Tests -Adding Tests -Optimise services/micro services

--- src/main/java/be/vdab/toys/repositories/OrderRepository.java
+++ src/main/java/be/vdab/toys/repositories/OrderRepository.java
@@ -2,12 +2,14 @@ package be.vdab.toys.repositories;
 
 import be.vdab.toys.domain.Order;
 import be.vdab.toys.domain.Status;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order,Long> {
+    @EntityGraph(Order.WITH_CUSTOMERSANDCOUNTRY)
     List<Order> findAll();
 //    List<Order> findAllInList(List<Long> idList);
 //    List<Order> findAllInListWithoutCustomersAndCountry(List<Long> idList);
